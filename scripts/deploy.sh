bfd70692f4941ba60001a857647ecd5383e97f26
André Roggeri Campos <a.roggeri.c@gmail.com>
2017-07-05 00:30:50 -0300

    Test deploy !

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,10 +6,20 @@ fi
 
 if [[ $TRAVIS_BRANCH == "master" ]]; then
   echo "Deploying to Production"
+  ng build --aot --env=prod
   dpl --provider=firebase --token=$FIREBASE_TOKEN --project=agendaodonto-29023
+else
+  echo "Skipping deploy to production. (Not on master branch)"
 fi
 
 if [[ $TRAVIS_BRANCH == "develop" ]]; then
   echo "Deploying to Staging"
+  ng build --aot -env=staging
   dpl --provider=firebase --token=$FIREBASE_TOKEN --project=agendaodontoweb-staging
+else
+  echo "Skipping deploy to production. (Not on develop branch)"
 fi
+
+echo "Deploying to Staging (Force deploy just for test!)"
+ng build --aot -env=staging
+dpl --provider=firebase --token=$FIREBASE_TOKEN --project=agendaodontoweb-staging
