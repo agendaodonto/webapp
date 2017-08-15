43d1bb5e37fa73d19d1852b10dba2854469a7ba6
André Roggeri Campos <a.roggeri.c@gmail.com>
2017-08-14 21:08:44 -0300

    Create account component (#6)

---
+++ src/app/account/account.module.ts
@@ -0,0 +1,23 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { AccountComponent } from './account.component';
+import { AuthGuard } from '../shared/guards/auth.guard';
+import { CommonModule } from '@angular/common';
+import { DentistService } from '../shared/services/dentist.service';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialAppModule } from '../shared/material.app.module';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+export const accountRoutes: Routes = [
+    { path: 'conta', component: AccountComponent, canActivate: [AuthGuard] }
+]
+
+@NgModule({
+    imports: [CommonModule, MaterialAppModule, FlexLayoutModule, ReactiveFormsModule, RouterModule],
+    declarations: [AccountComponent],
+    providers: [DentistService]
+})
+export class AccountModule {
+
+}
