2ea351188c33d48ea23d92ed76cfb2663a05bcd9
André Roggeri Campos <a.roggeri.c@gmail.com>
2017-09-09 11:55:02 -0300

    Auto focus on the first input using the AutFocusDirective

---
+++ src/app/shared/directives/directives.module.ts
@@ -0,0 +1,10 @@
+import { AutoFocusDirective } from './auto-focus.directive';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    declarations: [AutoFocusDirective],
+    exports: [AutoFocusDirective]
+})
+export class DirectivesModule {
+
+}
