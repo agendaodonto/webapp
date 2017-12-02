1ac4b56b13f44308e4b611719776026f75617d52
André Roggeri Campos <a.roggeri.c@gmail.com>
2017-12-01 23:23:12 -0200

    (feat): migrate clinic component to Material Table

--- src/app/clinic/clinic.component.ts
+++ src/app/clinic/clinic.component.ts
@@ -1,9 +1,12 @@
 import { ClinicService, IClinic } from 'app/clinic/clinic.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { ClinicFilter } from './clinic.service';
 import { IPaginateEvent } from '../shared/components/pager/datatable-pager.component';
 import { Router } from '@angular/router';
+import { DataSource, CollectionViewer } from '@angular/cdk/collections';
+import { Observable } from 'rxjs/Observable';
+import { MatPaginator } from '@angular/material';
 
 @Component({
     selector: 'app-clinic',
@@ -13,14 +16,16 @@ import { Router } from '@angular/router';
 export class ClinicComponent implements OnInit {
     clinics: IClinic[] = [];
     isLoading = false;
-    pageLimit = 10;
-    clinicCount = 0;
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+    dataSource: ClinicDataSource;
+    displayedColumns = ['name', 'actions']
 
     constructor(private clinicService: ClinicService, private router: Router) {
     }
 
     ngOnInit() {
-        this.getClinics(0);
+        // this.getClinics(0);
+        this.dataSource = new ClinicDataSource(this.clinicService, this.paginator);
     }
     getClinics(page: number) {
         this.isLoading = true;
@@ -31,15 +36,45 @@ export class ClinicComponent implements OnInit {
             .subscribe(
             response => {
                 this.clinics = response.results;
-                this.clinicCount = response.count;
             });
     }
-    view(clinic: { selected: Array<IClinic> }) {
-        this.router.navigate(['/clinicas/' + clinic.selected[0].id]);
+    view(clinic: IClinic) {
+        this.router.navigate(['/clinicas/' + clinic.id]);
     }
 
     paginate(paginateEvent: IPaginateEvent) {
         this.getClinics(paginateEvent.offset * paginateEvent.limit);
     }
+
+}
+
+
+class ClinicDataSource extends DataSource<IClinic> {
+    count = 0;
+    isLoading = true;
+
+    constructor(private clinicService: ClinicService, private paginator: MatPaginator) {
+        super();
+    }
+
+    connect(_collectionViewer: CollectionViewer): Observable<IClinic[]> {
+        const displayDataChages = [this.paginator.page];
+
+        return Observable.merge(...displayDataChages)
+            .startWith(null)
+            .switchMap(() => {
+                this.isLoading = true;
+                const clinicFilter = new ClinicFilter();
+                const offset = this.paginator.pageSize * this.paginator.pageIndex;
+                clinicFilter.setFilterValue('pageSize', this.paginator.pageSize.toString());
+                clinicFilter.setFilterValue('offset', offset.toString());
+                return this.clinicService.getAll(clinicFilter).map(response => {
+                    this.count = response.count;
+                    return response.results;
+                }).finally(() => this.isLoading = false);
+            })
+    }
+
+    disconnect(_collectionViewer: CollectionViewer): void { }
 
 }
