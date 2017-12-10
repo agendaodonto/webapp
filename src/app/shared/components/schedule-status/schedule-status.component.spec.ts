09fb25da60cb0d68b3e8510c25f6463c07201a43
André Roggeri Campos <a.roggeri.c@gmail.com>
2017-12-10 18:56:44 -0200

    fix: Failing ScheduleStatusComponent test

--- src/app/shared/components/schedule-status/schedule-status.component.spec.ts
+++ src/app/shared/components/schedule-status/schedule-status.component.spec.ts
@@ -1,16 +1,16 @@
-import { CommonModule } from '@angular/common';
-import { ScheduleStatusComponent } from './schedule-status.component';
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+
 import { MaterialAppModule } from 'app/shared/material.app.module';
+import { ScheduleStatusComponent } from './schedule-status.component';
 
-
-describe('ScheduleStatusComponent', () => {
+describe('Schedule Status Component', () => {
     let component: ScheduleStatusComponent;
     let fixture: ComponentFixture<ScheduleStatusComponent>;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            imports: [CommonModule, MaterialAppModule]
+            imports: [MaterialAppModule],
+            declarations: [ScheduleStatusComponent]
         }).compileComponents();
     }));
 
