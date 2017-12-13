1135dbeb3f23136fc231f9844bf81b52b79a7bd0
André Roggeri Campos <a.roggeri.c@gmail.com>
2017-12-13 00:22:37 -0200

    fix: Deploy only tags to production

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,21 +4,21 @@ if [[ ${TRAVIS_TEST_RESULT=0} == 1 ]]; then
   exit 1;
 fi
 
-if [[ $TRAVIS_BRANCH == "master" ]]; then
+if [[ ! -z $TRAVIS_TAG ]]; then
   echo "Deploying to Production"
   ng build --aot --env=prod
   firebase use --token $FIREBASE_TOKEN agendaodonto-29023
   firebase deploy --non-interactive --token $FIREBASE_TOKEN
 else
-  echo "Skipping deploy to production. (Not on master branch)"
+  echo "Skipping deploy to production. (Not a tag)"
 fi
 
-if [[ ! -z $TRAVIS_TAG ]]; then
+if [[ $TRAVIS_BRANCH == "develop" ]]; then
   echo "Deploying to Staging"
   ng build --aot -env=staging
   firebase use --token $FIREBASE_TOKEN agendaodontoweb-staging
   firebase deploy --non-interactive --token $FIREBASE_TOKEN
 else
-  echo "Skipping deploy to production. (Not a tag)"
+  echo "Skipping deploy to staging. (Not on develop branch)"
 fi
 
