27bd81902e004252042948b4e272079ab562a621
André Roggeri Campos <a.roggeri.c@gmail.com>
2017-12-18 23:48:26 -0200

    feat: Add useful information in the dashboard (Closes #124)

--- src/app/shared/components/shared-components.module.ts
+++ src/app/shared/components/shared-components.module.ts
@@ -2,11 +2,12 @@ import { MaterialAppModule } from '../material.app.module';
 import { NgModule } from '@angular/core';
 import { ScheduleStatusComponent } from 'app/shared/components/schedule-status/schedule-status.component';
 import { CommonModule } from '@angular/common';
+import { LoadingOverlayComponent } from 'app/shared/components/loading-overlay/loading-overlay.component';
 
 @NgModule({
     imports: [CommonModule, MaterialAppModule],
-    declarations: [ScheduleStatusComponent],
-    exports: [ScheduleStatusComponent],
+    declarations: [ScheduleStatusComponent, LoadingOverlayComponent],
+    exports: [ScheduleStatusComponent, LoadingOverlayComponent],
     providers: []
 })
 export class SharedComponentsModule {
