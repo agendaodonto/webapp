19af109cebf8b6e70de068529991de7103102b51
André Roggeri Campos <a.roggeri.c@gmail.com>
2018-01-31 22:15:25 -0200

    fix(Deploy): Remove pull request check from the deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,11 +4,6 @@ if [[ ${TRAVIS_TEST_RESULT=0} == 1 ]]; then
   exit 1;
 fi
 
-if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
-  echo "Skipping pull request"
-  exit 0;
-fi
-
 if [[ $TRAVIS_BRANCH == "master" ]]; then
   echo "Deploying to Production"
   ng build --env=prod --prod
