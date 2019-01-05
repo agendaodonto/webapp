afa74794bc6630fa8f9dae183a7f70090940aee2
André Roggeri Campos <a.roggeri.c@gmail.com>
2019-01-04 23:29:08 -0200

    Improve the build system (Closes #614)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,21 +4,17 @@ if [[ ${TRAVIS_TEST_RESULT=0} == 1 ]]; then
   exit 1;
 fi
 
-if [[ $TRAVIS_BRANCH == "master" ]]; then
+if [[ $1 == "production" ]]; then
   echo "Deploying to Production"
-  ng b -c=prod --prod=true
+  mv dist-prod dist
   firebase use --token $FIREBASE_TOKEN agendaodonto-29023
   firebase deploy --non-interactive --token $FIREBASE_TOKEN
-else
-  echo "Skipping deploy to production. (Not a tag)"
 fi
 
-if [[ $TRAVIS_BRANCH == "develop" ]]; then
+if [[ $1 == "staging" ]]; then
   echo "Deploying to Staging"
-  ng b -c=staging --prod=true
+  mv dist-staging dist
   firebase use --token $FIREBASE_TOKEN agendaodontoweb-staging
   firebase deploy --non-interactive --token $FIREBASE_TOKEN
-else
-  echo "Skipping deploy to staging. (Not on develop branch)"
 fi
 
