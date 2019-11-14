cd06b6f59415a50b9c90e20234ab0b0330f4ed22
André Roggeri Campos <a.roggeri.c@gmail.com>
2019-11-13 23:34:22 -0300

    feat: Allow changing the date in the dashboard

--- src/app/patient/patient.filter.ts
+++ src/app/patient/patient.filter.ts
@@ -9,6 +9,8 @@ export class PatientFilter extends BaseFilter {
             { name: 'phone', mapsTo: 'phone', value: null, type: 'filter' },
             { name: 'lastName', mapsTo: 'last_name', value: null, type: 'filter' },
             { name: 'fullName', mapsTo: 'full_name', value: null, type: 'filter' },
+            { name: 'createdAfter', mapsTo: 'created_0', value: null, type: 'filter' },
+            { name: 'createdBefore', mapsTo: 'created_1', value: null, type: 'filter' },
         );
         this.setFilterValue('orderBy', 'name');
     }
