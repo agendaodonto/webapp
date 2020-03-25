f331e9128890cd6c9c561c0518a3a6169d3f9ef7
André Roggeri Campos <a.roggeri.c@gmail.com>
2020-03-24 21:32:40 -0300

    fix: Schedule filter parameter

--- src/app/schedule/schedule.filter.ts
+++ src/app/schedule/schedule.filter.ts
@@ -4,8 +4,8 @@ export class ScheduleFilter extends BaseFilter {
     constructor() {
         super();
         this.fields.push(
-            { name: 'startDate', mapsTo: 'date_0', value: null, type: 'filter' },
-            { name: 'endDate', mapsTo: 'date_1', value: null, type: 'filter' },
+            { name: 'startDate', mapsTo: 'date_after', value: null, type: 'filter' },
+            { name: 'endDate', mapsTo: 'date_before', value: null, type: 'filter' },
             { name: 'status', mapsTo: 'status', value: null, type: 'filter' },
         );
         this.setFilterValue('orderBy', 'date');
